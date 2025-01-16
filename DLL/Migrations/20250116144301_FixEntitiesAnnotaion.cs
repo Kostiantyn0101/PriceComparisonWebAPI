a80699dbb111b20eeb8296324529d4dce7078ca3
kkosstiki <kkosstiki@gmail.com>
2025-01-16 16:47:36 +0200

    FixEntitiesAnnotaion

---
+++ DLL/Migrations/20250116144301_FixEntitiesAnnotaion.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DLL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixEntitiesAnnotaion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FeedbackText",
+                table: "Feedbacks",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Rating_Range",
+                table: "Feedbacks",
+                sql: "[Rating] BETWEEN 1 AND 5");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Rating_Range",
+                table: "Feedbacks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FeedbackText",
+                table: "Feedbacks",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
