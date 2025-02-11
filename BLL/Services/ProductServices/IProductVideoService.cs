71e8cc31fbd3c6cd8ffc7c6edcda7c8b4a42108c
kkosstiki <kkosstiki@gmail.com>
2025-02-11 19:47:12 +0200

    modify services: instruction, productvideo

--- BLL/Services/ProductServices/IProductVideoService.cs
+++ BLL/Services/ProductServices/IProductVideoService.cs
@@ -1,16 +1,18 @@
 ﻿using System.Linq.Expressions;
 using Domain.Models.DBModels;
+using Domain.Models.Request.Products;
 using Domain.Models.Response;
+using Domain.Models.Response.Products;
 
 namespace BLL.Services.ProductService
 {
     public interface IProductVideoService
     {
-        Task<OperationDetailsResponseModel> CreateAsync(ProductVideoDBModel model);
-        Task<OperationDetailsResponseModel> UpdateAsync(ProductVideoDBModel entity);
-        Task<OperationDetailsResponseModel> DeleteAsync(int id);
+        Task<OperationResultModel<bool>> CreateAsync(ProductVideoCreateRequestModel model);
+        Task<OperationResultModel<bool>> UpdateAsync(ProductVideoUpdateRequestModel entity);
+        Task<OperationResultModel<bool>> DeleteAsync(int id);
         IQueryable<ProductVideoDBModel> GetQuery();
-        Task<IEnumerable<ProductVideoDBModel>> GetFromConditionAsync(Expression<Func<ProductVideoDBModel, bool>> condition);
+        Task<IEnumerable<ProductVideoResponseModel>> GetFromConditionAsync(Expression<Func<ProductVideoDBModel, bool>> condition);
         Task<IEnumerable<ProductVideoDBModel>> ProcessQueryAsync(IQueryable<ProductVideoDBModel> query);
     }
 }
