f390802dfbc7f139d0bab0ddb59a3f02bb26fc62
kkosstiki <kkosstiki@gmail.com>
2025-02-12 17:37:38 +0200

    refactor chain related category, add updatedto

---
+++ Domain/Models/Request/Categories/RelatedCategoryUpdateRequestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Request.Categories
+{
+    public class RelatedCategoryUpdateRequestModel
+    {
+        public int OldCategoryId { get; set; }
+        public int OldRelatedCategoryId { get; set; }
+
+        public int NewCategoryId { get; set; }
+        public int NewRelatedCategoryId { get; set; }
+    }
+}
