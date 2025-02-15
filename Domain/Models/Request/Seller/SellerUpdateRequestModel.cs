1dc174ea44ba8daeec826947809b145abef3c016
kkosstiki <kkosstiki@gmail.com>
2025-02-15 21:57:54 +0200

    create DTO models for seller

---
+++ Domain/Models/Request/Seller/SellerUpdateRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Request.Seller
+{
+    public class SellerUpdateRequestModel
+    {
+        public int Id { get; set; }
+        public string ApiKey { get; set; }
+        public string? LogoImageUrl { get; set; }
+        public string? WebsiteUrl { get; set; }
+        public bool IsActive { get; set; }
+        public int UserId { get; set; }
+    }
+}
