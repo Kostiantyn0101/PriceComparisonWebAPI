8116592ed400f197a88e53e3d1c23e00b3f882ce
Andrey Sosnov <donkihot1984@gmail.com>
2025-02-16 22:09:19 +0200

    CharacteristicGroup validation updated

---
+++ PriceComparisonWebAPI/Infrastructure/Validation/CharacteristicGroupCreateRequestModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models.Request.Products;
+using FluentValidation;
+
+namespace PriceComparisonWebAPI.Infrastructure.Validation
+{
+    public class CharacteristicGroupCreateRequestModelValidator : AbstractValidator<CharacteristicRequestModel>
+    {
+        public CharacteristicGroupCreateRequestModelValidator()
+        {
+            RuleFor(x => x.Id)
+                 .GreaterThan(0).WithMessage("Characteristic group id must be greater than 0");
+
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(100).WithMessage("Title length must be less than 100 characters");
+        }
+    }
+}
