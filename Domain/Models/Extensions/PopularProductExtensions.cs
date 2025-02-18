5db6ceade09393ba98cefb93f1d0ada45c865d31
Andrey Sosnov <donkihot1984@gmail.com>
2025-02-18 23:16:51 +0200

    popular products image url updated

---
+++ Domain/Models/Extensions/PopularProductExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models.Response.Products;
+
+namespace Domain.Models.Extensions
+{
+    public static class PopularProductExtensions
+    {
+        public static void ApplyServerUrl(this PopularProductResponseModel product, string serverBaseUrl)
+        {
+            if (!string.IsNullOrEmpty(product.ImageUrl))
+            {
+                product.ImageUrl = $"{serverBaseUrl.TrimEnd('/')}/{product.ImageUrl.TrimStart('/')}";
+            }
+        }
+    }
+}
