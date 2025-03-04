1a65b735c5453dedee964ddd3a1bbf730113c85a
Andrey Sosnov <donkihot1984@gmail.com>
2025-03-04 23:24:46 +0200

    ProductGroup servese, models, controller and validation added

---
+++ Domain/Models/Request/Products/ProductGroupUpdateRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models.Request.Products
+{
+    public class ProductGroupUpdateRequestModel
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductGroupId { get; set; }
+    }
+
+}
---
+++ PriceComparisonWebAPI/Infrastructure/Validation/Product/ProductGroupCreateRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace PriceComparisonWebAPI.Infrastructure.Validation.Product
+{
+    using Domain.Models.Request.Products;
+    using FluentValidation;
+
+    public class ProductGroupCreateRequestValidator : AbstractValidator<ProductGroupCreateRequestModel>
+    {
+        public ProductGroupCreateRequestValidator()
+        {
+            RuleFor(x => x.ProductId)
+                .GreaterThan(0).WithMessage("ProductId must be greater than 0.");
+            RuleFor(x => x.ProductGroupId)
+                .NotEmpty().WithMessage("ProductGroupId is required.");
+        }
+    }
+
+}
