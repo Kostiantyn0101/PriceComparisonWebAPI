c8bb4d37afa9fc8db91d68839a48e863d0afbbc7
kkosstiki <kkosstiki@gmail.com>
2025-03-09 23:32:40 +0200

    addMigration

--- Domain/Models/DBModels/FilterDBModel.cs
+++ Domain/Models/DBModels/FilterDBModel.cs
@@ -8,7 +8,7 @@ namespace Domain.Models.DBModels
         public string Title { get; set; }
         public string ShortTitle { get; set; }
         public string Description { get; set; }
-        public int CharacteristicId { get; set; }
+        public int? CharacteristicId { get; set; }
         public CharacteristicDBModel Characteristic { get; set; }
         public string Operator { get; set; } // "==", ">=", "<=", ">", "<", "!="
         public string Value { get; set; }
