c7918b9eec0a9cb8e72db3e1e4e9c21faded729d
Andrey Sosnov <donkihot1984@gmail.com>
2025-03-13 23:56:55 +0200

    Role endpoints added

--- PriceComparisonWebAPI/Program.cs
+++ PriceComparisonWebAPI/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.FileProviders;
 using PriceComparisonWebAPI.Infrastructure;
 using PriceComparisonWebAPI.Infrastructure.DependencyInjection;
