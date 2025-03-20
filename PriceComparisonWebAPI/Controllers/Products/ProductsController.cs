f20abef21b70781c4ff9314dc4b838374f118f0d
kkosstiki <kkosstiki@gmail.com>
2025-03-20 19:01:18 +0200

    fix product return

--- PriceComparisonWebAPI/Controllers/Products/ProductsController.cs
+++ PriceComparisonWebAPI/Controllers/Products/ProductsController.cs
@@ -106,7 +106,7 @@ namespace PriceComparisonWebAPI.Controllers.Products
                 return GeneralApiResponseModel.GetJsonResult(AppErrors.General.CreateError, StatusCodes.Status400BadRequest, result.ErrorMessage);
             }
 
-            return GeneralApiResponseModel.GetJsonResult(AppSuccessCodes.CreateSuccess, StatusCodes.Status200OK);
+            return GeneralApiResponseModel.GetJsonResult(AppSuccessCodes.CreateSuccess, StatusCodes.Status200OK, null, result.Data);
         }
 
 
